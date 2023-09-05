af12134eaa4702498731f574b03ede8228113f85
IanaLois <>
2023-09-05 17:21:59 +0000

    Implement string validation for username input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Variables
     const usernameForm = document.getElementById('username-form');
+    const usernameInput = document.getElementById('username');
+    const usernameDisplay = document.getElementById('username-display');
 
+    /* Displays username input once it passes string validation of only letters,
+     numbers and special characters "-" or "_" and hides the starting 
+     home page by proceeding to the Rules and Difficulty page */
     usernameForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        const startPage = document.querySelector('.start-page');
-        const rulesAndDifficulty = document.querySelector('.rules-and-difficulty');
-        const Rules = document.querySelector('.rules');
-        const Difficulty = document.querySelector('.choose-difficulty');
+        const enteredUsername = usernameInput.value;
 
-        startPage.style.display = 'none';
-        rulesAndDifficulty.style.display = 'block';
-        Rules.style.display = 'block';
-        Difficulty.style.display = 'block';
+        if (/^[a-zA-Z0-9_-]+$/.test(enteredUsername)) {
+            usernameDisplay.textContent = enteredUsername;
+
+            const startPage = document.querySelector('.start-page');
+            const rulesAndDifficulty = document.querySelector('.rules-and-difficulty');
+            const Rules = document.querySelector('.rules');
+            const Difficulty = document.querySelector('.choose-difficulty');
+
+            startPage.style.display = 'none';
+            rulesAndDifficulty.style.display = 'block';
+            Rules.style.display = 'block';
+            Difficulty.style.display = 'block';
+        } else {
+            alert('Your username can only include letters, numbers, hyphens, or underscores');
+        }
     });
 });
